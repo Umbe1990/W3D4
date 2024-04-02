4808a8da61a113129a2763c45cdc3dd83e10e912
umberto conedera <161523775+Umbe1990@users.noreply.github.com>
2024-04-02 18:42:47 +0200

    consegna

---
+++ s3p2-scaricabile.js
@@ -0,0 +1,82 @@
+// Il tuo compito è creare un sito e-commerce per Smartphones. 
+// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
+// Non sono richieste funzionalità di carrello/cassa.
+// - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
+// - Completa gli altri esercizi con JS
+
+//ESERCIZI IN HTML
+
+// ESERCIZIO 1: Inserisci un tag h1 con il nome del tuo negozio
+// ESERCIZIO 2: Aggiungi una tabella con i 5 prodotti più in vista del tuo negozio
+// ESERCIZIO 3: Aggiungi per ogni prodotto un'immagine, un titolo, una descrizione e un prezzo
+// ESERCIZIO 4: Per ogni elemento della tabella aggiungi un link di Amazon al prodotto esistente
+// ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
+// ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
+
+//ESERCIZI IN JS
+// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+
+
+function modificah1(){
+    let H1element=document.getElementById("title")
+    H1element.textContent="umberto"
+    let divtitle=document.querySelectorAll(".titolo")
+    divtitle.forEach(function(elemet){
+        elemet.style.backgroundColor="orange"
+    }
+    )
+}
+
+
+function modificabody(){
+let bodyelement=document.getElementById("paginaIntera")
+bodyelement.style.backgroundColor="orange"
+}
+
+
+function modificaPrezzo(){
+    let prezzoelement=document.querySelectorAll(".cards")
+    prezzoelement.forEach(function(elemet){
+        elemet.style.backgroundColor="yellow"
+    }
+    )
+}
+
+function modificaPrezzo2(){
+    let prezzoelement2=document.querySelectorAll(".card-title")
+    prezzoelement2.forEach(function(elemet){
+        elemet.style.backgroundColor="yellow"
+        elemet.textContent="umberto"
+        
+    }
+    )
+
+
+
+}
+
+function operzione(numeri){
+    let number1=Document.getElementById("number1")
+    let number2=Document.getElementById("number2")
+    let tipoOperzione=Document.getElementById("tipoOperzione")
+    let risultato=0
+
+        switch(tipoOperzione){
+            case "addizione":
+            risultato=number1+number2
+            break;
+        }
+
+        alert(risultato)
+        
+
+
+
+
+
+}
